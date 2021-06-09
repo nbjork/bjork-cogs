292024cf9a42f041129027c8cb40d8aa73e20143
Nathan Bjork <nbjork@gmail.com>
2021-06-09 14:57:11 -0500

    Update stonk.py

--- stonk.py
+++ stonk.py
@@ -1,13 +1,13 @@
 from redbot.core import commands
 
-class MyCog(commands.Cog):
+class Stonks(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def stonks(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
