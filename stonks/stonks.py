bb92c1031fd0e7c164df2a1900eee16c5d39f4b9
Nathan Bjork <nbjork@gmail.com>
2021-06-09 15:06:41 -0500

    Update stonks.py

--- stonks/stonks.py
+++ stonks/stonks.py
@@ -1,1 +1,14 @@
 
+from redbot.core import commands
+
+class Stonks(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def stonks(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
