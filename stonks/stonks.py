4617d2cf4386ce53b35797d5b3906b690d7ddc41
Nathan Bjork <nbjork@gmail.com>
2021-06-09 15:45:36 -0500

    Update stonks.py

--- stonks/stonks.py
+++ stonks/stonks.py
@@ -1,8 +1,7 @@
 
 from redbot.core import commands
-from discord.ext import commands
 
-class Stonks:
+class Stonks(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
@@ -13,4 +12,3 @@ class Stonks:
         """This does stuff!"""
         # Your code will go here
         await ctx.send("I can do stuff!")
-        await self.bot.say('BRO!')
